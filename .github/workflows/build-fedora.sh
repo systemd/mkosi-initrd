48848cef76cffa4d1f64d947c40275cc3e8c11d6
Frantisek Sumsal <frantisek@sumsal.cz>
2022-01-26 22:20:39 +0100

    ci: basic iSCSI boot test

--- .github/workflows/build-fedora.sh
+++ .github/workflows/build-fedora.sh
@@ -4,8 +4,13 @@ set -eux
 set -o pipefail
 
 # shellcheck disable=SC2206
-PHASES=(${@:-DEPS INITRD})
+PHASES=(${@:-DEPS INITRD_BASIC})
 SYSTEMD_LOG_OPTS="systemd.log_target=console udev.log_level=info systemd.default_standard_output=journal+console"
+MKOSI_CACHE="/var/tmp/mkosiinitrd$(</etc/machine-id).cache"
+
+if [[ ! -d "$MKOSI_CACHE" ]]; then
+    mkdir -v "$MKOSI_CACHE"
+fi
 
 if rpm -q kernel-core >/dev/null; then
     # Can't use `uname -r`, since we're in a container
@@ -16,44 +21,111 @@ for phase in "${PHASES[@]}"; do
     case "$phase" in
         DEPS)
             echo "Installing necessary dependencies"
-            dnf -y install dnf-plugins-core kernel-core mkosi python3-pyxattr qemu-kvm systemd zstd
+            dnf -y install \
+                dnf-plugins-core \
+                dnsmasq \
+                iproute \
+                kernel-core \
+                mkosi \
+                python3-pyxattr \
+                qemu-kvm \
+                scsi-target-utils \
+                systemd \
+                zstd
             ;;
-        INITRD)
+        INITRD_BASIC)
             rm -fr mkosi.output
             mkdir mkosi.output
-
-            python3 -m mkosi --default fedora.mkosi \
+            # Build a basic initrd
+            python3 -m mkosi --cache "$MKOSI_CACHE" \
+                             --default fedora.mkosi \
                              --image-version="$KVER" \
                              --environment=KERNEL_VERSION="$KVER" \
                              -f build
             # Check if the image was indeed generated
             stat "mkosi.output/initrd_$KVER.cpio.zstd"
 
-            # Basic boot test
-            timeout -k 10 5m qemu-kvm -m 512 -smp "$(nproc)" -nographic \
-                                      -initrd "mkosi.output/initrd_$KVER.cpio.zstd" \
-                                      -kernel "/usr/lib/modules/$KVER/vmlinuz" \
-                                      -append "rd.systemd.unit=systemd-poweroff.service rd.debug $SYSTEMD_LOG_OPTS console=ttyS0"
-            ;;
-        INITRD_FULLBOOT)
-            if [[ ! -e "mkosi.output/initrd_$KVER.cpio.zstd" ]]; then
-                echo >&2 "Missing initrd image (run the INITRD phase first)"
-                exit 1
-            fi
-
-            # Build a basic image to test the initrd with
+            # Build a basic OS image to test the initrd with
+            rm -fr _rootfs
             mkdir _rootfs
             pushd _rootfs
             # shellcheck source=/dev/null
             source <(grep -E "(ID|VERSION_ID)" /etc/os-release)
-            mkosi --distribution="$ID" --release="$VERSION_ID" --format=gpt_btrfs --output=rootfs.img
+            mkosi --cache "$MKOSI_CACHE" \
+                  --distribution="$ID" \
+                  --release="$VERSION_ID" \
+                  --format=gpt_btrfs \
+                  --output=rootfs.img
             popd
 
+            # Sanity check if the initrd is bootable
+            timeout -k 10 5m qemu-kvm -m 512 -smp "$(nproc)" -nographic \
+                                      -initrd "mkosi.output/initrd_$KVER.cpio.zstd" \
+                                      -kernel "/usr/lib/modules/$KVER/vmlinuz" \
+                                      -append "rd.systemd.unit=systemd-poweroff.service rd.debug $SYSTEMD_LOG_OPTS console=ttyS0"
+
+            # Boot the initrd with an OS image
             timeout -k 10 5m qemu-kvm -m 1024 -smp "$(nproc)" -nographic \
                                       -initrd "mkosi.output/initrd_$KVER.cpio.zstd" \
                                       -kernel "/usr/lib/modules/$KVER/vmlinuz" \
                                       -drive "format=raw,cache=unsafe,file=_rootfs/rootfs.img" \
                                       -append "root=LABEL=root rd.debug $SYSTEMD_LOG_OPTS console=ttyS0 systemd.unit=systemd-poweroff.service systemd.default_timeout_start_sec=240"
+
+            # Cleanup
+            rm -fr mkosi.output _rootfs
+            ;;
+        INITRD_ISCSI)
+            rm -fr mkosi.output
+            mkdir mkosi.output
+            # Build the initrd with iSCSI support
+            python3 -m mkosi --cache "$MKOSI_CACHE" \
+                             --default fedora.mkosi \
+                             --package="NetworkManager,iscsi-initiator-utils" \
+                             --image-version="$KVER" \
+                             --environment=KERNEL_VERSION="$KVER" \
+                             -f build
+            ## Check if the image was indeed generated
+            stat "mkosi.output/initrd_$KVER.cpio.zstd"
+
+            # Build a basic image to test the initrd with
+            rm -fr _rootfs
+            mkdir _rootfs
+            pushd _rootfs
+            # shellcheck source=/dev/null
+            source <(grep -E "(ID|VERSION_ID)" /etc/os-release)
+            mkosi --cache "$MKOSI_CACHE" \
+                  --distribution="$ID" \
+                  --release="$VERSION_ID" \
+                  --format=gpt_btrfs \
+                  --output=rootfs.img
+            popd
+
+            # Setup a local iSCSI target
+            target_name="iqn.2022-01.com.example:iscsi.initrd.test"
+            pgrep tgtd || /usr/sbin/tgtd
+            tgtadm --lld iscsi --op new --mode target --tid=1 --targetname "$target_name"
+            tgtadm --lld iscsi --op new --mode logicalunit --tid 1 --lun 1 -b "$PWD/_rootfs/rootfs.img"
+            tgtadm --lld iscsi --op update --mode logicalunit --tid 1 --lun 1
+            tgtadm --lld iscsi --op bind --mode target --tid 1 -I ALL
+            tgtadm --lld iscsi --op show --mode target
+
+            ip link add name initrd0 type bridge
+            ip addr add 10.10.10.1/24 dev initrd0
+            ip link set initrd0 up
+            dnsmasq --interface=initrd0 --bind-interfaces --dhcp-range=10.10.10.10,10.10.10.100
+            grep -q initrd0 /etc/qemu/bridge.conf || echo "allow initrd0" >>/etc/qemu/bridge.conf
+
+            iscsi_cmdline="ip=dhcp netroot=iscsi:10.10.10.1::::$target_name"
+            timeout -k 10 5m qemu-kvm -m 1024 -smp "$(nproc)" -nographic -nic bridge,br=initrd0 \
+                                      -initrd "mkosi.output/initrd_$KVER.cpio.zstd" \
+                                      -kernel "/usr/lib/modules/$KVER/vmlinuz" \
+                                      -append "$iscsi_cmdline root=LABEL=root rd.debug $SYSTEMD_LOG_OPTS console=ttyS0 systemd.unit=systemd-poweroff.service systemd.default_timeout_start_sec=240"
+
+            # Cleanup
+            tgtadm --lld iscsi --op delete --mode target --tid=1
+            pkill -INT tgtd
+            pkill dnsmasq
+            rm -fr mkosi.output _rootfs
             ;;
         SYSEXT)
             rm -fr mkosi.output
@@ -75,6 +147,9 @@ for phase in "${PHASES[@]}"; do
                              --package='!*,openssh-server'
             # Check if the image was indeed generated
             stat "mkosi.output/initrd_$KVER-ssh.raw"
+
+            # Cleanup
+            rm -fr mkosi.output
             ;;
         *)
             echo >&2 "Unknown phase '$phase'"
